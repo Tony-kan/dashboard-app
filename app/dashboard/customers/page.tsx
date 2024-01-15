df2d4cee8b4b56f57b6273457500b8d23b8c1e12
Tony Kanyamuka <tonykanyamuka@gmail.com>
2024-01-15 09:06:34 +0200

    implemented authentication

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,12 @@
 
 import { Metadata } from 'next';
+import Pagination from '@/app/ui/invoices/pagination';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/customers/table';
+import { TableRowSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import { fetchCustomers } from '@/app/lib/data';
+import { lusitana } from '@/app/ui/fonts';
 
 export const metadata: Metadata = {
   title: 'Cutomers',
@@ -7,5 +14,27 @@ export const metadata: Metadata = {
 
 
 export default function page(){
-    return <p>Customers page</p>;
+
+  // const query = searchParams?.query || '';
+  // const currentPage = Number(searchParams?.page) || 1;
+  
+  // const totalPages = await fetchInvoicesPages(query);
+  // const customers = await fetchCustomers();
+
+    return <div>customers</div>
+    //   <div className="w-full">
+    //   <div className="flex w-full items-center justify-between">
+    //     <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
+    //   </div>
+    //   <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+    //     <Search placeholder="Search invoices..." />
+      
+    //   </div>
+    //    <Suspense key={query + currentPage} fallback={<TableRowSkeleton />}>
+    //     <Table  customers={}/>
+    //   </Suspense>
+    //   <div className="mt-5 flex w-full justify-center">
+    //     <Pagination totalPages={totalPages} />
+    //   </div>
+    // </div>
 }
